c8ed797da55ca42cad4ed6b2e2af0779893d18b8
Veronica Smigielski <veronicasmigielski@gmail.com>
2021-03-04 12:13:05 +0100

    Updated to comment in the challenge

--- FirstLastDigitSum/src/Main.java
+++ FirstLastDigitSum/src/Main.java
@@ -1,3 +1,9 @@
+// Coding Exercise
+// Write a method named sumFirstAndLastDigit with one parameter of type int called number
+// The method needs to find the first and last digit of the parameter number passed to the method
+// using a loop and return the sum of the first and last digit of that number
+// If the number is negative, then the method needs to return -1 to indicate an invalid value
+
 public class Main {
     public static void main(String[] args) {
         // Setup a variable to accept return value (4)
