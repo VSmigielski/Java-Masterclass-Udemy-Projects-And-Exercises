4f06b60d045f7339db48bc54b5bd8768a1d13e07
Veronica <veronicasmigielski@gmail.com>
2021-05-12 12:48:32 +0200

    numberToWords method, turns digit of each number into a word written as entered

---
+++ numberToWords/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Setup a variable to accept return value (4)
+        numberToWords.numberToWords(123);
+        numberToWords.numberToWords(1010);
+        numberToWords.numberToWords(1000);
+        numberToWords.numberToWords(-12);
+    }
+}
