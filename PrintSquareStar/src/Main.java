917e4939e93dac0de8c13a1577f87df627711ac7
Veronica <veronicasmigielski@gmail.com>
2021-05-12 15:04:58 +0200

    Print a formation based on the number entered using columns and rows

---
+++ PrintSquareStar/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        printSquareStar.printSquareStar(5);
+        printSquareStar.printSquareStar(8);
+        printSquareStar.printSquareStar(0);
+    }
+}
