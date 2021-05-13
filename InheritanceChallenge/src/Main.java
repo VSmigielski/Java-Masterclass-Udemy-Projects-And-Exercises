b2cefa9531fc8a4be2a8abe4460e8e56a22a0645
Veronica <veronicasmigielski@gmail.com>
2021-05-13 22:57:44 +0200

    2 classes, one parent Circle, one child Cylinder to calculate the volume of a cylinder

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Create a new instance of MiniCooper class
         MiniCooper minicooper = new MiniCooper(36);
 
+        // Use the instance method steer
         minicooper.steer(45);
 
+        // Use the instance method accelerate
         minicooper.accelerate(30);
-
         minicooper.accelerate(20);
-
+        // Use the instance method accelerate to decelerate
         minicooper.accelerate(-42);
     }
 }
