49066ce5cf7c5577f994745318d3c46a21ead574
Veronica <veronicasmigielski@gmail.com>
2021-05-14 09:28:28 +0200

    Added another class to follow the proper exercise naming conventions and checks

--- EncapsulationChallengeExercise/src/Main.java
+++ EncapsulationChallengeExercise/src/Main.java
@@ -12,5 +12,19 @@ public class Main {
         Printer printer1 = new Printer(20, true);
         printer1.printPage(2);
         printer1.printPage(4);
+
+        TimPrinter printer2 = new TimPrinter(50, true);
+        System.out.println("initial page count = " + printer2.getPagesPrinted());
+        int pagesPrinted = printer2.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer2.getPagesPrinted());
+        pagesPrinted = printer2.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer2.getPagesPrinted());
+
+        TimPrinter printer3 = new TimPrinter(50, false);
+        System.out.println("initial page count = " + printer2.getPagesPrinted());
+        pagesPrinted = printer3.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer3.getPagesPrinted());
+        pagesPrinted = printer3.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer3.getPagesPrinted());
     }
 }
