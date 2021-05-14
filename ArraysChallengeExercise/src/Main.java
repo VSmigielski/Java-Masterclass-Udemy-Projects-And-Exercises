88c0b13dda473ae4720bf3c14f884def98492d60
Veronica <veronicasmigielski@gmail.com>
2021-05-14 18:43:16 +0200

    Accept user input to put into an array, print the array, sort the array into descending order

---
+++ ArraysChallengeExercise/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+
+        printArray(myIntegers);
+
+        copyArrayInteger(myIntegers);
+
+        printArray(sortArray(copyArrayInteger(myIntegers)));
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element: " + i + ", Value: " + array[i]);
+        }
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static int[] sortArray(Integer[] array) {
+        Arrays.sort(array, Collections.reverseOrder());
+        return copyArrayBack(array);
+    }
+
+    public static Integer[] copyArrayInteger(int[] array) {
+        Integer[] newArray = new Integer[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+
+        return newArray;
+    }
+
+    public static int[] copyArrayBack(Integer[] array) {
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+
+        return newArray;
+    }
+
+    // Another way to handle this information without copying&sorting separately
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length-1; i++) {
+                if (sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
