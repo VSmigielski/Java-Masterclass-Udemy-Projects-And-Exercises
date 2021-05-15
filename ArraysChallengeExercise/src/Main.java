39808cbd675b62b5eac03209cb4fbce6b2f2e3c8
Veronica <veronicasmigielski@gmail.com>
2021-05-15 12:29:36 +0200

    Interview Question in Java

--- ArraysChallengeExercise/src/Main.java
+++ ArraysChallengeExercise/src/Main.java
@@ -10,8 +10,6 @@ public class Main {
 
         printArray(myIntegers);
 
-        copyArrayInteger(myIntegers);
-
         printArray(sortArray(copyArrayInteger(myIntegers)));
 
     }
@@ -60,10 +58,13 @@ public class Main {
 
     // Another way to handle this information without copying&sorting separately
     public static int[] sortIntegers(int[] array) {
-        int[] sortedArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            sortedArray[i] = array[i];
-        }
+        // Built in way to copy array
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
 
         boolean flag = true;
         int temp;
---
+++ HelloWorldInterviewStyleQuestion/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String []args){
+        // 30Hello World
+        System.out.println(10 + 20 + "Hello World");
+        // Hello World1020
+        System.out.println("Hello World" + 10 + 20);
+    }
+}
+
